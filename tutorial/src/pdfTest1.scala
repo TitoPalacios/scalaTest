4a8e0fa51a84cbc5f67d87f15946193786a1102b
Tito <vgbpalacios@gmail.com>
2016-09-15 11:38:16 +0200

    Using tables ....

--- tutorial/src/pdfTest1.scala
+++ tutorial/src/pdfTest1.scala
@@ -1,27 +1,134 @@
 /**
-  * Created by Tito Palacios on 14/09/16.
+  * Created by Tito Palacios
   * Date: 14/09/16
-  * Time: 19:19
-  * ........
-  * ........
+  * Sandbox to play with iText and Scala
   */
-import java.io.FileOutputStream
+import java.io.{FileOutputStream, IOException}
 
-import com.itextpdf.text.Document
-import com.itextpdf.text.DocumentException
-import com.itextpdf.text.Paragraph
-import com.itextpdf.text.pdf.PdfWriter
-import java.io.FileOutputStream
+import com.itextpdf.text._
+import com.itextpdf.text.pdf.PdfPageLabels.PdfPageLabelFormat
+import com.itextpdf.text.pdf.{PdfPCell, PdfPTable, PdfWriter}
+
 
 object pdfTest1 {
+  @throws(classOf[DocumentException])
+  @throws(classOf[IOException])
   def main(args: Array[String]): Unit = {
-    println("First example using PDF libraries... ")
-    val sFilePDF  =   "/tmp/pdfTest1.pdf"
-    createPDF (sFilePDF)
+    val t0 = System.nanoTime()
+    println("Working with PDF libraries... ")
+    createPDFFirstTable()
+    val t1 = System.nanoTime()
+    println("Elapsed time: "+(t1-t0)/1e9D+" sec.")
   }
 
-  def createPDF(sFilePDF: String): Unit = {
-    println("...Generating file"+sFilePDF)
+  @throws(classOf[DocumentException])
+  @throws(classOf[IOException])
+  def createPDFFirstTable(): Unit = {
+    // Using a custom page size
+    val sFilePDF = "tutorial/data/TestFirstTable.pdf"
+    val document = new Document()
+    PdfWriter.getInstance(document, new FileOutputStream(sFilePDF))
+    println("...Generating file "+sFilePDF)
+
+    val table = new PdfPTable(3)
+    val cell1 = new PdfPCell(new Phrase("Cell with colspan 3"))
+    cell1.setColspan(3)
+    table.addCell(cell1)
+    val cell2 = new PdfPCell(new Phrase("Cell with rowspan 2"))
+    cell2.setRowspan(2)
+    table.addCell(cell2)
+
+    table.addCell("row 1; cell 1")
+    table.addCell("row 1; cell 2")
+    table.addCell("row 2; cell 1")
+    table.addCell("row 2; cell 2")
+
+    val table2 = new PdfPTable(4)
+    val text = new Phrase("Special")
+
+    //text.setFont()
+    //text.setFont(new Font("Helvetica"))
+    val cell3 = new PdfPCell(text)
+    cell3.setBorder(1)
+    cell3.setBorderColor(BaseColor.WHITE)
+    cell3.setBackgroundColor(BaseColor.BLUE)
+
+
+    table2.addCell("col1")
+    table2.addCell(cell3)
+    table2.addCell("col3")
+    table2.addCell("col4")
+    val cell4 = new PdfPCell(table2)
+    cell4.setColspan(3)
+    table.addCell(cell4)
+
+    document.open()
+
+    document.add(table)
+
+    document.close()
+  }
+
+  @throws(classOf[DocumentException])
+  @throws(classOf[IOException])
+  def createPDFMirroredMargins(): Unit = {
+    // Using a custom page size
+    val sFilePDF = "tutorial/data/TestMirroredMargins.pdf"
+    val document = new Document(PageSize.A5)
+    PdfWriter.getInstance(document, new FileOutputStream(sFilePDF))
+    document.setMargins(36, 72,108,180)
+    document.setMarginMirroring(true)
+    println("...Generating file "+sFilePDF)
+    document.open()
+    document.add(new Paragraph("The left margin of this odd page is 36pt (0.5 inch); " +
+      "the right margin 72pt (1 inch); " +
+      "the top margin 108pt (1.5 inch); " +
+      "the bottom margin 180pt (2.5 inch)."))
+    val paragraph = new Paragraph()
+    paragraph.setAlignment(Element.ALIGN_JUSTIFIED)
+    for (i<- 0 to 20) {
+      paragraph.add("Hello World! Hello People!" +
+                    "Hello Sky! Hello Sun! Hello Moon! Hello Stars!!")
+    }
+    document.add(paragraph)
+    document.add(new Paragraph(
+      "The right margin of this even page is 36pt;" +
+      "the left margin 72pt"
+    ))
+    document.close()
+  }
+
+  @throws(classOf[DocumentException])
+  @throws(classOf[IOException])
+  def createPDFLandscape(): Unit = {
+    // Using a custom page size
+    val sFilePDF = "tutorial/data/TestLandscape.pdf"
+    val document = new Document(PageSize.A4.rotate())
+    PdfWriter.getInstance(document, new FileOutputStream(sFilePDF))
+    println("...Generating file "+sFilePDF)
+    document.open()
+    document.add(new Paragraph("Hello in a Landscape World !!!"))
+    document.close()
+  }
+
+  @throws(classOf[DocumentException])
+  @throws(classOf[IOException])
+  def createPDFSpecialSize(): Unit = {
+    // Using a custom page size
+    val sFilePDF = "tutorial/data/TestSpecialSize.pdf"
+    val pagesize = new Rectangle(216f, 720f)
+    val document = new Document(pagesize, 36f, 72f, 108f, 180f)
+    PdfWriter.getInstance(document, new FileOutputStream(sFilePDF))
+    println("...Generating file "+sFilePDF)
+    document.open()
+    document.add(new Paragraph("Hello World !!!"+"Hello Sky! Hello stars!! Hello everybody"))
+    document.close()
+  }
+
+  @throws(classOf[DocumentException])
+  @throws(classOf[IOException])
+  def createPDF1(sFilePDF: String): Unit = {
+    println("...Generating file "+sFilePDF)
     val document = new Document()
     PdfWriter.getInstance(document, new FileOutputStream(sFilePDF))
     document.open()
